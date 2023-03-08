6d1db37a1a0c1cfc9a7bc403661d4834e05f05f5
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-03-08 13:22:18 -0500

    Moved cd command into its own file.

---
+++ headers/cd.h
@@ -0,0 +1,16 @@
+/*
+
+Nicholas DiGirolamo
+cd.h
+
+Created for University of Delaware CISC361 - Operating Systems
+Assignment 3 - Simple Shell
+
+*/
+
+#ifndef CD_H
+#define CD_H
+
+void cd (char *args[], int arg_count);
+
+#endif
---
+++ srcs/cd.c
@@ -0,0 +1,32 @@
+/*
+
+Nicholas DiGirolamo
+cd.c
+
+Created for University of Delaware CISC361 - Operating Systems
+Assignment 3 - Simple Shell
+
+A simple cd command.
+
+*/
+
+#include "cd.h"
+
+void cd (char *args[], int arg_count) {
+
+    if (arg_count < 2) {
+        char *ptr = getenv("HOME");
+        char *path = malloc((strlen(ptr) + 1) * sizeof(char));
+        strcpy(path, ptr);
+        if (chdir(path) == -1) {
+            perror("[cd] Error");
+        }
+        free(path);
+    } else {
+        char *path = args[1];
+        if (chdir(path) == -1) {
+            perror("[cd] Error");
+        }
+    }
+
+}
