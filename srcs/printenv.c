6d49a05c6807bce81809cf4b039571403496035f
Nicholas DiGirolamo <nsdigirolamo@gmail.com>
2023-03-24 17:49:31 -0400

    Added argument to only print a single variable. Also error codes and return values.

--- srcs/printenv.c
+++ srcs/printenv.c
@@ -6,18 +6,47 @@ printenv.c
 Created for University of Delaware CISC361 - Operating Systems
 Assignment 3 - Simple Shell
 
-A simple printenv command.
+A simple printenv command. Prints a list of environment variables, or just a
+single environment variable given as an argument. On success, returns 0. On
+failure, returns -1 and sets errno accordingly.
+
+EINVAL - Argument could not be found.
+E2BIG - Too many arguments.
 
 */
 
+#include <errno.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "printenv.h"
 
 extern char **environ;
 
-void printenv () {
-    for (int i = 0; environ[i] != NULL; i++) {
-        fprintf(stdout, "%s\n", environ[i]);
+int printenv (int argc, char *argv[]) {
+
+    if (argc < 2) {
+
+        for (int i = 0; environ[i] != NULL; i++) {
+            fprintf(stdout, "%s\n", environ[i]);
+        }
+        return 0;
+
+    } else if (argc == 2) {
+
+        char *env_ptr = getenv(argv[1]);
+        if (env_ptr) {
+            fprintf(stdout, "%s\n", env_ptr);
+            return 0;
+        } else {
+            errno = EINVAL;
+            return -1;
+        }
+
+    } else {
+
+        errno = E2BIG;
+        return -1;
+
     }
 }
